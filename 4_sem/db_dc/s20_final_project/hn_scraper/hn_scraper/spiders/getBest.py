98301e104ad13e10693901a7d563de64d19f5643
mitsiu-carreno <mitsiu.carreno@gmail.com>
2024-09-07 22:54:39 -0600

    scrap first level comments

--- 4_sem/db_dc/s20_final_project/hn_scraper/hn_scraper/spiders/getBest.py
+++ 4_sem/db_dc/s20_final_project/hn_scraper/hn_scraper/spiders/getBest.py
@@ -13,17 +13,34 @@ class GetbestSpider(scrapy.Spider):
             getComments = scrapy.Request(
                 f"https://hnrss.org/replies?id={comments_id}",
                 callback=self.parseComment, 
-                cb_kwargs=dict(title=entry.xpath(".//title/text()").get())
+                cb_kwargs=dict(
+                    title=entry.xpath(".//title/text()").get(),
+                    description=entry.xpath(".//description/text()").get(),
+                    pubDate=entry.xpath(".//pubDate/text()").get(),
+                    link=entry.xpath(".//link/text()").get(),
+                    dc_creator=entry.xpath(".//*[name()='dc:creator']/text()").get(),
+                    guid=entry.xpath(".//guid/text()").get()
+                )
             )
             yield getComments
 
-    def parseComment(self, response, title):
+    def parseComment(self, response, title, description, pubDate, link, dc_creator, guid):
         comments = []
         for comment in response.xpath("/rss/channel/item"):
             comments.append({
-                "title": comment.xpath(".//title/text()").get()
+                "title": comment.xpath(".//title/text()").get(),
+                "description": comment.xpath(".//description/text()").get(),
+                "pubDate": comment.xpath(".//pubDate/text()").get(),
+                "link": comment.xpath(".//link/text()").get(),
+                "dc_creator": comment.xpath(".//*[name()='dc:creator']/text()").get(),
+                "guid": comment.xpath(".//guid/text()").get()
             })
         yield {
             "title": title,
-            "comments": comments
+            "description": description,
+            "pubDate": pubDate,
+            "link": link,
+            "dc_creator": dc_creator,
+            "comments": comments,
+            "guid": guid
         }
